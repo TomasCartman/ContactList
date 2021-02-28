5fde182cd480e18547d57f58d9920621cf20d751
Tomas Cartman <marco12y@outlook.com>
2021-02-27 23:18:55 -0300

    add function to perform a click and call the intent with the phone number to the dialer | locale to portuguese

--- app/src/main/java/com/blackpineapple/correiocontatos/recyclerview/ContactHolder.kt
+++ app/src/main/java/com/blackpineapple/correiocontatos/recyclerview/ContactHolder.kt
@@ -1,10 +1,13 @@
 package com.blackpineapple.correiocontatos.recyclerview
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.blackpineapple.correiocontatos.Contact
@@ -17,6 +20,7 @@ class ContactHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickLis
 
     init {
         itemView.setOnClickListener(this)
+        itemView.setOnLongClickListener(this)
     }
 
     fun bind(contact: Contact) {
@@ -27,10 +31,15 @@ class ContactHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickLis
     }
 
     override fun onClick(v: View?) {
-
+        val intent = Intent().apply {
+            action = Intent.ACTION_DIAL
+            data = Uri.parse("tel:" + contact.number.toString())
+        }
+        v?.context?.startActivity(intent)
     }
 
     override fun onLongClick(v: View?): Boolean {
-
+        Toast.makeText(itemView.context, "onLongClick", Toast.LENGTH_SHORT).show()
+        return true
     }
 }

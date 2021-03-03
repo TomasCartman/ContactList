ff761755884b6ab4a464e80ceb3fd3f99af28567
Tomas Cartman <marco12y@outlook.com>
2021-03-03 00:36:03 -0300

    add funtion to update a contact

--- app/src/main/java/com/blackpineapple/correiocontatos/ContactFormFragment.kt
+++ app/src/main/java/com/blackpineapple/correiocontatos/ContactFormFragment.kt
@@ -8,10 +8,14 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import kotlin.IllegalStateException
 
+const val ARG_NAME = "name"
+const val ARG_NUMBER = "number"
+
 class ContactFormFragment : DialogFragment() {
     interface ContactFormDialogListener {
         fun onDialogPositiveClick(dialog: DialogFragment, name: String, number: String)
-        fun onDialogNegativeClick(dialog: DialogFragment)
+        fun onDialogNegativeClick(dialog: DialogFragment, name: String)
+        fun onDialogNeutralClick(dialog: DialogFragment)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -21,6 +25,19 @@ class ContactFormFragment : DialogFragment() {
 
             val view = inflater.inflate(R.layout.dialog_contact, null)
 
+            if(arguments != null) {
+                val nameEditText = view.findViewById<EditText>(R.id.name_editText)
+                val numberEditText = view.findViewById<EditText>(R.id.phone_editText)
+                val name = arguments!!.get(ARG_NAME) as String
+                val number = arguments!!.get(ARG_NUMBER) as Long
+                nameEditText.apply {
+                    setText(name)
+                    isEnabled = false
+                }
+                numberEditText.setText(number.toString())
+            }
+
+            builder.setTitle(R.string.contact)
             builder.setView(view)
                     .setPositiveButton(R.string.save, DialogInterface.OnClickListener {
                         _, _ ->
@@ -32,12 +49,31 @@ class ContactFormFragment : DialogFragment() {
                             fragment -> (fragment as ContactFormDialogListener).onDialogPositiveClick(this, name, number)
                         }
                     })
-                    .setNegativeButton(R.string.cancel, DialogInterface.OnClickListener {
-                        _, _ -> targetFragment?.let {
-                            fragment -> (fragment as ContactFormDialogListener).onDialogNegativeClick(this)
+                    .setNeutralButton(R.string.cancel, DialogInterface.OnClickListener {
+                        _, _ ->
+                        targetFragment?.let {
+                            fragment -> (fragment as ContactFormDialogListener).onDialogNeutralClick(this)
+                        }
+                    })
+                    .setNegativeButton(R.string.delete, DialogInterface.OnClickListener {
+                        _, _ ->
+                        val nameEditText = view.findViewById<EditText>(R.id.name_editText)
+                        val name = nameEditText.text.toString()
+                        targetFragment?.let {
+                            fragment -> (fragment as ContactFormDialogListener).onDialogNegativeClick(this, name)
                         }
                     })
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    companion object {
+        fun newInstance(name: String, number: Long): ContactFormFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_NAME, name)
+                putSerializable(ARG_NUMBER, number)
+            }
+            return ContactFormFragment().apply { arguments = args }
+        }
     }
 }
--- app/src/main/java/com/blackpineapple/correiocontatos/ContactListFragment.kt
+++ app/src/main/java/com/blackpineapple/correiocontatos/ContactListFragment.kt
@@ -1,9 +1,13 @@
 package com.blackpineapple.correiocontatos
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.view.animation.AnimationUtils
 import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.DialogFragment
@@ -11,12 +15,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.blackpineapple.correiocontatos.recyclerview.ContactAdapter
 
 private const val TAG = "ContactListFragment"
+const val DIALOG_CONTACT_FORM = "DialogContactForm"
 
 class ContactListFragment : Fragment(), ContactFormFragment.ContactFormDialogListener {
     private lateinit var contactListViewModel: ContactListViewModel
@@ -116,8 +121,75 @@ class ContactListFragment : Fragment(), ContactFormFragment.ContactFormDialogLis
         }
     }
 
-    override fun onDialogNegativeClick(dialog: DialogFragment) {
-        // Do nothing
+    override fun onDialogNegativeClick(dialog: DialogFragment, name: String) {
+        // Delete the contact
         Toast.makeText(context, "onDialogNegativeClick", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDialogNeutralClick(dialog: DialogFragment) {
+        // Do nothing
+        Toast.makeText(context, "onDialogNeutralClick", Toast.LENGTH_SHORT).show()
+    }
+
+    private inner class ContactHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener, View.OnLongClickListener {
+        private lateinit var contact: Contact
+        private val textViewContactName: TextView = view.findViewById(R.id.contact_name_text_view)
+        private val textViewContactPhone: TextView = view.findViewById(R.id.contact_phone_text_view)
+
+        init {
+            itemView.setOnClickListener(this)
+            itemView.setOnLongClickListener(this)
+        }
+
+        fun bind(contact: Contact) {
+            this.contact = contact
+
+            textViewContactName.text = contact.name
+            textViewContactPhone.text = contact.number.toString()
+        }
+
+        override fun onClick(v: View?) {
+            val intent = Intent().apply {
+                action = Intent.ACTION_DIAL
+                data = Uri.parse("tel:" + contact.number.toString())
+            }
+            v?.context?.startActivity(intent)
+        }
+
+        override fun onLongClick(v: View?): Boolean {
+            ContactFormFragment.newInstance(contact.name, contact.number).apply {
+                setTargetFragment(this@ContactListFragment, 1)
+                show(this@ContactListFragment.parentFragmentManager , DIALOG_CONTACT_FORM)
+            }
+            return true
+        }
+    }
+
+    private inner class ContactAdapter : androidx.recyclerview.widget.ListAdapter<Contact, ContactHolder>(ContactDiffCallback()) {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactHolder {
+            val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.contact, parent, false)
+
+            return ContactHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ContactHolder, position: Int) {
+            val contact = getItem(position)
+
+            val animation = AnimationUtils.loadAnimation(holder.itemView.context, R.anim.fade_animation)
+            holder.itemView.startAnimation(animation)
+
+            holder.bind(contact)
+        }
+    }
+
+    class ContactDiffCallback : DiffUtil.ItemCallback<Contact>() {
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
     }
 }
